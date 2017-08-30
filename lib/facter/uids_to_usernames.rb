3b6384c64c777d5aeb568922ed162a54dcd9f3de
Berin Smaldon <berin.smaldon1@nhs.net>
2017-08-30 11:47:23 +0100

    fixes syntax and sorts hash

--- lib/facter/uids_to_usernames.rb
+++ lib/facter/uids_to_usernames.rb
@@ -2,6 +2,6 @@
 Facter.add(:uids_usernames) do
   setcode do
     passwd_values = File.open('/etc/passwd', 'r').map { |line| line.split ':', 4 }
-    Hash[ passwd_values.map { |values| [ values[0], values[2].to_i ] }
+    Hash[ passwd_values.map { |values| [ values[0], values[2].to_i ] } ].sort
   end
 end
