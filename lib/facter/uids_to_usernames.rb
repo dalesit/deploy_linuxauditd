882abb3cbad1feff140f1c8f30f12d3fbf2df564
Berin Smaldon <berin.smaldon1@nhs.net>
2017-08-30 15:12:53 +0100

    misuse of rescue, fixed to only be slightly terrible

--- lib/facter/uids_to_usernames.rb
+++ lib/facter/uids_to_usernames.rb
@@ -1,7 +1,7 @@
 
 Facter.add(:uids_usernames) do
   setcode do
-    passwd_values = File.open('/etc/passwd', 'r').map { |line| line.split ':', 4 } rescue return {}
+    passwd_values = File.open('/etc/passwd', 'r').map { |line| line.split ':', 4 } rescue []
     Hash[ passwd_values.map { |values| [ values[2].to_i, values[0] ] } ].sort
   end
 end
