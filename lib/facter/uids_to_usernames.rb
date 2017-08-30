7b2bc38264346a2b157ca8f6f851c70947c3f18b
Berin Smaldon <berin.smaldon1@nhs.net>
2017-08-30 11:31:00 +0100

    experimental fact to collect posix UIDs and usernames

---
+++ lib/facter/uids_to_usernames.rb
@@ -0,0 +1,7 @@
+
+Facter.add(:uids_usernames) do
+  setcode do
+    passwd_values = File.open('/etc/passwd', 'r').map { |line| line.split ':', 4 }
+    Hash[ passwd_values.map { |values| [ values[0], values[2].to_i ] }
+  end
+end
